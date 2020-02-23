d4f03dae7a0eab0409ec14b8689d136260ee98b6
valeriikocherha <48161975+valeriikocherha@users.noreply.github.com>
2020-02-23 23:34:30 +0200

    Add files via upload

---
+++ lab_rob_1.py
@@ -0,0 +1,16 @@
+import math
+
+x = input("x=")
+x = float(x)
+a = input("a=")
+a = float(a)
+a = math.radians(a)
+print(a)
+
+if (abs(x)<=2) | (x<0):
+    print("Ошибка значения числа х, введите x заново")
+if (a == math.pi) | (a == 0):
+    print("Ошибка значения числа a, на 0 делить нельзя")
+else:
+    ans = (math.log1p(x**3 - 8) + 1/math.sin(a))
+    print(ans)
