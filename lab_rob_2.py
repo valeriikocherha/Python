a0262b57b4689d197ed193b84b47e4fbb904587e
valeriikocherha <48161975+valeriikocherha@users.noreply.github.com>
2020-02-29 23:12:31 +0200

    Update and rename lab_rob_2.py to task_2_13.py

--- lab_rob_2.py
+++
@@ -1,9 +0,0 @@
-num_1 = float(input("num_1="))
-num_2 = float(input("num_2="))
-num_3 = float(input("num_3="))
-
-def func(x):
-    return min(x)
-
-ans = func([num_1, num_2, num_3])
-print(ans)
---
+++ task_2_13.py
@@ -0,0 +1,9 @@
+num = float(input("num="))
+
+if num >= 0:
+    num *= 3
+    print(num)
+
+if num < 0:
+    num = num**2
+    print(num)
