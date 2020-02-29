1a5b1670c6cafd17dd9898b9d1acfab9b1fde11b
valeriikocherha <48161975+valeriikocherha@users.noreply.github.com>
2020-02-29 23:13:32 +0200

    Update and rename lab_rob_1.py to task_1_13.py

--- task_1_13.py
+++ task_1_13.py
@@ -7,10 +7,16 @@ a = float(a)
 a = math.radians(a)
 print(a)
 
-if (abs(x)<=2) | (x<0):
+while a < 0:
+    a = 2 * (math.pi)
+while a >= 2 * (math.pi):
+    a -= 2 * (math.pi)
+print(a)
+
+if (abs(x) <= 2) | (x < 0):
     print("Ошибка значения числа х, введите x заново")
 if (a == math.pi) | (a == 0):
     print("Ошибка значения числа a, на 0 делить нельзя")
 else:
-    ans = (math.log1p(x**3 - 8) + 1/math.sin(a))
+    ans = (math.log1p(x ** 3 - 8) + 1 / math.sin(a))
     print(ans)
